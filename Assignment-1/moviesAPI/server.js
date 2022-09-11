adb7681263b8a147debea0455c37e91c99225476
Ravni Arador <ravarador@gmail.com>
2022-09-10 21:09:48 -0400

    Added moviesDB module.

--- Assignment-1/moviesAPI/server.js
+++ Assignment-1/moviesAPI/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
+const MoviesDB = require("./modules/moviesDB");
+const db = new MoviesDB();
 
 const app = express();
 
@@ -11,9 +13,13 @@ app.use(cors());
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    res.json({ message: "API Listening"});
+    res.json({ message: "API Listening" });
 })
 
-app.listen(HTTP_PORT, () => {
-    console.log("Server listening on: " + HTTP_PORT);
-})
+db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log(`server listening on: ${HTTP_PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
