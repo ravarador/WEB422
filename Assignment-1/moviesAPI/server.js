0b8f39b495bae7bd7b4e5ea43573fcef35822b06
Ravni Arador <ravarador@gmail.com>
2022-09-11 17:38:42 -0400

    Added get movies route

--- Assignment-1/moviesAPI/server.js
+++ Assignment-1/moviesAPI/server.js
@@ -16,6 +16,27 @@ app.get("/", (req, res) => {
     res.json({ message: "API Listening" });
 })
 
+// POST /api/movies
+
+// GET /api/movies
+app.get("/api/movies/", (req, res) => {
+    var page = req.query.page;
+    var perPage = req.query.perPage;
+    var title = req.query.title;
+
+    db.getAllMovies(page, perPage, title).then(data => {
+        res.json(data);
+    }).catch((err) => {
+        res.status(404).json({message: err});
+    })
+})
+
+// GET /api/movies/:id
+
+// PUT /api/movie
+
+// DELETE /api/movies
+
 db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
     app.listen(HTTP_PORT, () => {
         console.log(`server listening on: ${HTTP_PORT}`);
